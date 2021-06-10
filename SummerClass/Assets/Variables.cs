ce6f8872fb3553ed4caa711f4e7b6df0f8c2ff1e
Anthony Romrell <anthonyromrell@mac.com>
2021-06-10 16:43:46 -0600

    Added Variables Script

---
+++ SummerClass/Assets/Variables.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    public int highScore= 100;
+    public string playerName = "Anthony";
+    public string dialogue = "Hello world";
+}
